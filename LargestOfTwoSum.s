0b03d048d177bca83304e55c0990ead1dfe077ad
Lucky24seven <117457291+Lucky24seven@users.noreply.github.com>
2023-03-01 19:46:34 -0500

    Add files via upload
    Guess I did the first assignment wrong after re reading the prompt.  This should be the 2nd MIPS32 QtSPIM program.  Will take in 3 user inputs and sum the largest two.

---
+++ LargestOfTwoSum.s
@@ -0,0 +1,116 @@
+# Title: Homework6 Problem 1		Filename: LargestOfTwoSum.s
+# Author: William Lawecki		Date: 2/28/2023
+# Description: This program will take 3 user inputs. After the inputs are entered
+# the program will sum the two largest values.
+# Input: Intergers input by user
+# Output: Integer sum, largest, and smallest values entered.
+
+##### Data Segment #####
+
+.data
+
+str1: .asciiz "Welcome, this program will sum the two largest numbers that you enter.  Please enter 3 integer values.\n"
+str2: .asciiz "\nPlease enter the first number: "
+str3: .asciiz "\nPlease enter the second number: "
+str4: .asciiz "\nEnter the last number, please: "
+str5: .asciiz "\nThe sum of the largest two integers entered is: "
+
+##### Code Segment #####
+
+.text
+.globl main
+
+main: 
+
+li $v0, 4				# initial user prompt on starting program
+la $a0, str1
+syscall
+
+li $v0, 4				# asking for first user input
+la $a0, str2
+syscall
+
+li $v0, 5           		# read in input to reg $t0
+syscall
+move $t0, $v0
+
+li $v0, 4				# asking for second user input
+la $a0, str3
+syscall
+
+li $v0, 5           		# read in input to reg $t1
+syscall
+move $t1, $v0
+
+li $v0, 4				# asking for last user input
+la $a0, str4
+syscall
+   
+li $v0, 5		           	# read in input to reg $t2
+syscall
+move $t2, $v0
+
+# calculate sum and store result in $t3
+
+add $t3, $zero, $zero		# Clearing register value
+add $t3, $t0, $t1
+add $t3, $t3, $t2
+
+      # finding the smallest
+      
+# assume input 1 is smallest
+
+move $t4, $t0
+
+# if input 2 < than the current smallest 
+
+bgt $t4, $t1, one
+b continue
+
+one:
+      
+move $t4, $t1			# make input 2 the new smallest
+
+continue:
+
+# if input 3 < than the current smallest 
+
+bgt $t4, $t2, two
+b continue_2
+
+two:
+
+move $t4, $t2			# make input 3 the new smallest
+
+continue_2:
+
+# remove the smallest from the sum of all three
+
+sub $t3, $t3, $t4 
+
+li $v0, 4				# System call to report our User's sum
+la $a0, str5
+syscall
+
+li $v0, 1
+move $a0, $t3
+syscall
+
+#li $v0, 4				# Prompting user on smallest value entered
+#la $a0, str6
+#syscall
+
+#li $v0, 1				# Reporting smallest value
+#move $a0, $t4
+#syscall
+
+#li $v0, 4				# Prompting user for largest value entered
+#la $a0, str7
+#syscall
+
+#li $v0, 1				# Reporting largest value
+#move $a0, $t1
+#syscall
+
+li $v0, 10				# System call to end the program
+syscall
